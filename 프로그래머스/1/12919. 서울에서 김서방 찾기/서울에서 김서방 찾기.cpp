e5d07fb8b6fc367650be647ccb50149c6c0b0c98
Hyun <beevvv12@gmail.com>
2025-01-08 09:40:10 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.05 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cpp
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cpp
@@ -7,7 +7,7 @@ string solution(vector<string> seoul)
 {
     for (int i = 0; i < seoul.size(); i++)  // 배열 순회
     {
-        if (seoul[i] == "Kim")  // 순회하며 Kim 인지 확인
+        if (seoul[i] == "Kim")  // 순회하며 요소가 Kim 인지 확인
         {
             // 결과 문자열 생성 후 반환
             return "김서방은 " + to_string(i) + "에 있다";
